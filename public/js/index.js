c0adf954f28976c634317fde4fa9cec27c601527
System Administrator <root@MacBook-Pro-de-Dennis.local>
2017-01-04 17:36:46 -0500

    .

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function functionName() {
+  $('.parallax').parallax();
+  $('.slider').slider({full_width: true});
+  $('.scrollspy').scrollSpy();
+  $('.target').pushpin({
+      top: 0 ,
+      bottom: 1000 ,
+      offset: 0
+    });
+
+});
